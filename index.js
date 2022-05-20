6ccf4f31af99d746e80f8acce594edea827d9816
Eliza Joanna Kikowska <eliza.joanna.kikowska@zalando.de>
2022-05-20 16:26:49 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var darkButton = document.getElementById("darkButton");
+
+function darkMode (){
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+darkButton.addEventListener("click", darkMode);
+
+
+
+var button = document.getElementById("buttonup");
+
+function toTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTo({top:0, behavior:"smooth"});
+    }
+
+    buttonup.addEventListener("click", toTop);
+
+var menuButton = document.getElementById("menuButton");
+function menuFun(){
+    var htmlBody1 = document.body;
+    htmlBody1.classList.toggle("visibility")
+}
+
+menuButton.addEventListener("click", menuFun)
